ff0db3fc99229f77ac98cb59f9c21fe227120b29
Amir <dahmouni_amir@hotmail.fr>
2023-08-28 12:07:09 +0100

    change port

--- src/service/mailService.ts
+++ src/service/mailService.ts
@@ -5,7 +5,7 @@ export async function sendMail(name: string, email: string, message: string) {
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     secure: true,
     auth: {
       user: process.env.EMAIL_USER,
