ad0b43324453ca4bf191249352c6809caa766988
Amir <dahmouni_amir@hotmail.fr>
2023-08-28 12:46:56 +0100

    change secure

--- src/service/mailService.ts
+++ src/service/mailService.ts
@@ -6,7 +6,7 @@ export async function sendMail(name: string, email: string, message: string) {
     service: 'gmail',
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
